5de1bae9554ba6f36579c80a2560fc5f59e9d94e
Aaron Brooks <abrooks@systemsbiology.org>
2015-03-06 18:29:59 -0800

    ggbweb formatting fcns

---
+++ tmp_formatting.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Temp GGBweb python formating functions"""
+
+__author__ = "Aaron Brooks"
+__copyright__ = "Copyright 2014, cMonkey2"
+__credits__ = ["Aaron Brooks"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Aaron Brooks"
+__email__ = "brooksan@uw.edu"
+__status__ = "Development"
